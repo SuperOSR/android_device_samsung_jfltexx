791ef97392b6bd1d18595e28567be622aff6493d
superatmel <superatmel@gmail.com>
2013-09-12 16:43:21 +0200

    Delete files

--- osr.mk
+++ osr.mk
@@ -9,6 +9,9 @@ $(call inherit-product, vendor/osr/config/nfc_enhanced.mk)
 # Inherit some common OSR stuff.
 $(call inherit-product, vendor/osr/config/common_full_phone.mk)
 
+# Inherit Google Apps.
+$(call inherit-product, vendor/google/full.mk)
+
 PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=jfltexx TARGET_DEVICE=jfltexx BUILD_FINGERPRINT="samsung/jfltexx/jflte:4.2.2/JDQ39/I9505XXUAMDE:user/release-keys" PRIVATE_BUILD_DESC="jfltexx-user 4.2.2 JDQ39 I9505XXUAMDE release-keys"
 
 PRODUCT_DEVICE := jfltexx
